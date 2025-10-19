ea0eebd42b5d8ba73b183cf9af0a6780c6410acc
Cole Johnson <coledjohn@gmail.com>
2025-10-19 14:57:32 -0600

    Initial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+# NOTE: this first line is a dumb hack meant to produce
+# compile flags for my lsp to ignore things handled by CMake
+cmake -S . -G "Unix Makefiles" -B build
+# compile the project
+cmake --build build 
+# exec final binary 
+build/l-systems
