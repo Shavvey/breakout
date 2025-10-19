7549c92d36f523c4ab9be22841056e6342f5daee
Cole Johnson <coledjohn@gmail.com>
2025-10-19 16:45:08 -0600

    Making basic graphics object (vecs and rects)

---
+++ src/rectangle.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_SRC_RECTANGLE_H_
+#define INCLUDE_SRC_RECTANGLE_H_
+
+typedef struct {
+  int xpos;
+  int ypos;
+  int width;
+  int height;
+} Rectangle;
+
+#endif  // INCLUDE_SRC_RECTANGLE_H_
---
+++ src/vector.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDE_SRC_VECTOR_H_
+#define INCLUDE_SRC_VECTOR_H_
+
+typedef struct {
+  int dx;
+  int dy;
+} Vector2D;
+
+// FUNCTION PROTOTYPES
+
+#endif  // INCLUDE_SRC_VECTOR_H_
